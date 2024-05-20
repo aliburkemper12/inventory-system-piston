815dda61a7dc3a5098a2e8d592c39e5161885b5b
ali burkemper <aliburkemper12@gmail.com>
2024-05-20 14:03:36 -0500

    issue with show all button

--- src/fakemain.py
+++ src/fakemain.py
@@ -1,17 +1,31 @@
 import sqlite3
-from flask import Flask, render_template
+import inventory
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 def get_db_connection():
     conn = sqlite3.connect('inventory.db')
     conn.row_factory = sqlite3.Row
+    inventory.create_db()
+    
     return conn
 
-
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
     conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM item').fetchall()
+    cursor = conn.cursor()
+    if request.method == "POST":
+        name = request.form.get("fname")
+        data = cursor.execute('SELECT * FROM item WHERE name = ?', (name,)).fetchall()
+ 
+        return render_template("index.html", data=data)
+        
+
+    data = cursor.execute('SELECT * FROM item').fetchall()
     conn.close()
-    return render_template('index.html', item=item)
+    
+    return render_template("index.html", data=data)
+
+if __name__ == "__main__":
+    app.run(port = 5000)
