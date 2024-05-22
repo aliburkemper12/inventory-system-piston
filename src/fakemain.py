41327e311a211b43615aab94a808d7921e88796e
ali burkemper <aliburkemper12@gmail.com>
2024-05-22 07:52:17 -0500

    edit is now working

--- src/fakemain.py
+++ src/fakemain.py
@@ -41,22 +41,38 @@ def edit():
         
     return render_template("edit.html", data=data)
 
-@app.route("/ajax_edit", methods=["POST"])
-def ajax_edit():
+@app.route("/ajax_edit_n", methods=["POST"])
+def ajax_edit_n():
+    data = {}
     conn = get_db_connection()
     cursor = conn.cursor()
     name = request.form.get("name")
     new_name = request.form.get("new_name")
+    new_quant = request.form.get("new_quant")
+    new_desc = request.form.get("new_desc")
     
-    print(name)
-    print(new_name)
-    inventory.update_name(conn, name, new_name)
+    if new_name != "":
+        print('doing new name:')
+        command = 'UPDATE item SET name = ? WHERE name = ?;'
+        cursor.execute(command, (new_name, name))
+        conn.commit()
+        name = new_name
+    
+    if new_quant != "":
+        print('doing new quant:')
+        command = 'UPDATE item SET quantity = ? WHERE name = ?;'
+        cursor.execute(command, (new_quant, name))
+        conn.commit()
+    
+    if new_desc != "":
+        print('doing new desc:')
+        command = 'UPDATE item SET description = ? WHERE name = ?;'
+        cursor.execute(command, (new_desc, name))
+        conn.commit()
+    
     data = cursor.execute('SELECT * FROM item WHERE name = ?', (name,)).fetchall()
-        
     conn.close()
-        
-    # return render_template("edit.html", data=data)
-    return jsonify(item_name=new_name)
+    return render_template("edit.html", data=data)
 
 if __name__ == "__main__":
     app.run(port = 5000)
